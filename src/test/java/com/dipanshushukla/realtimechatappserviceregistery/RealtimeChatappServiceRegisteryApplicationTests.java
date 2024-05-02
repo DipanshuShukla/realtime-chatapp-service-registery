3b13d844eb126a2839402a6a3fe771d2c74c3274
Dipanshu Shukla <shuklascientist5@gmail.com>
2024-05-03 05:11:59 +0530

    initial commit

---
+++ src/test/java/com/dipanshushukla/realtimechatappserviceregistery/RealtimeChatappServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipanshushukla.realtimechatappserviceregistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeChatappServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
